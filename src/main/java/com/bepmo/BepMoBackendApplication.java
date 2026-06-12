f67d295ec9117908e678487938de254d114e1138
pantq1711 <hongan.gv10@gmail.com>
2026-06-12 23:06:01 +0700

    init: bootstrap Spring Boot project

---
+++ src/main/java/com/bepmo/BepMoBackendApplication.java
@@ -0,0 +1,13 @@
+package com.bepmo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BepMoBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BepMoBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bepmo/BepMoBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bepmo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BepMoBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
